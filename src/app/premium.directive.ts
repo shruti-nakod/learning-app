6dd91f9c9be544db1812dee71a546b0ea89f539d
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-09 00:01:38 +0530

    done

--- src/app/premium.directive.ts
+++ src/app/premium.directive.ts
@@ -1,14 +1,17 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appPremium]'
 })
 export class PremiumDirective {
-  //  Directive is a declarable
-  //  Every use of directive creates an instance
-  //  It is not advisable to change the selector of a directive
-  //  as it can cause conflicts with HTML attributes of built -in directives
-  constructor(private host: ElementRef) {
-    this.host.nativeElement.style.color = 'red';
+  //  @HostBinding('domProperty')
+  @HostBinding('className')
+  get css() {
+    return 'font-weight-bold';
+  }
+
+  @HostBinding('style.color')
+  get color() {
+    return 'blue';
   }
 }
