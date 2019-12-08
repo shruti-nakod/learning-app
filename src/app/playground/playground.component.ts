f1722d7677a9dbe436dfa448d4dc7b8582df02c0
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 22:38:26 +0530

    done

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -6,17 +6,14 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit {
-  //  @ViewChild('templateVariable') is a decorator for view query.
-  //  Here static property determines when the query is resolved.
-  //  Change detection cycle has a profound effect on view query.
-  //  For this demonstration, we query it before the change detection.
   @ViewChild('para', { static: true }) pElement: ElementRef;
 
   constructor() {}
 
   ngOnInit() {
-    //  pElement contains the native element which is the DOM node
-    //  We can mutate the node's properties
-    console.log('<p>', this.pElement);
+    this.pElement.nativeElement.className = 'font-weight-bold';
+    this.pElement.nativeElement.style.color = 'green';
+    this.pElement.nativeElement.style.textDecoration = 'underline';
+    this.pElement.nativeElement.style.fontSize = '30px';
   }
 }
