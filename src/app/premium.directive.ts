2ff03bb7459cda2779793cf82153b830769d50f5
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-08 23:56:36 +0530

    done

--- src/app/premium.directive.ts
+++ src/app/premium.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appPremium]'
@@ -8,7 +8,7 @@ export class PremiumDirective {
   //  Every use of directive creates an instance
   //  It is not advisable to change the selector of a directive
   //  as it can cause conflicts with HTML attributes of built -in directives
-  constructor() {
-    console.log('Directive created');
+  constructor(private host: ElementRef) {
+    console.log('Directive created', host);
   }
 }
